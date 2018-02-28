74e9a6f252bf09b8765e849b299fc624395f8bf3
Alec Spencer <aspencer18@my.bcit.ca>
2018-02-28 10:28:30 -0800

    completed version of passwd_analyzer_w_docstrings.py with accompanying test files.

--- docstrings/passwd_analyzer_w_docstrings.py
+++ docstrings/passwd_analyzer_w_docstrings.py
@@ -1,22 +1,26 @@
-#! /bin/python3
+#! /usr/bin/env
 '''
 passwd_analyzer_w_docstrings.py
 Alec Spencer
 aspencer18@my.bcit.ca
 February 26, 2018
-v0.5
+v1.0
 '''
 
 def split_group_line(line: str) -> list:
     '''
     Splits a string into a list individual strings
 
-    Parameters: line - a single line from /etc/group file
-    Returns: a list of group data for an individual group
+    Args:
+        line (str): a single line from /etc/group file
     
-    >>> split_group_line('root:x:0:0:root:/root:/bin/bash\n')
-    ['root', 'x', '0', '0', 'root', '/bin/bash\n']
+    Returns:
+        line (list): a list of group data for an individual group
+    
+    Example:
 
+        >>> split_group_line('root:x:0:0:root:/root:/bin/bash\\n')
+        ['root', 'x', '0', '0', 'root', '/root', '/bin/bash\\n']
     '''
 
     line = line.split(":")
@@ -28,12 +32,15 @@ def parse_groups(file_path='/etc/group') -> list:
     Opens a file in the format of /etc/group and makes a nested list where each line is a list entry,
     and each line is further split into a list of individual strings.
 
-    Parameters: file path (string) with a default of /etc/group
-    Returns: nested list of field seperated group data containing all supplemental users
+    Args:
+        file_path (str): a file name from which to read group data, with a default of /etc/group
 
-    >>> parse_groups('./parsely.list')
-    [['wheel', 'x', '10', 'bigmoocow,littlemoocow\n'], ['tcpdump', 'x', '72', '\n'], ['bigmoocow', 'x', '1000', 'bigmoocow\n']]
+    Returns:
+        grouplist (list): a nested list of field seperated group data containing all supplemental users
 
+    Example:
+        >>> parse_groups('./group.truncated')
+        [['wheel', 'x', '10', 'bigmoocow,littlemoocow\\n'], ['tcpdump', 'x', '72', '\\n'], ['bigmoocow', 'x', '1000', 'bigmoocow\\n']]
     '''
     grouplist = []
     
@@ -52,12 +59,16 @@ def is_user_account(user_data: list) -> bool:
     '''
     Takes a list as input and checks if the third element is a valid linux UID, returning true or false.
 
-    Parameters: user_data - list comprised of user data from single line of /etc/passwd
-    Returns: True if is a user account (includng root) otherwise false
-
-    >>> is_user_account(['root', 'x', '0', '0', 'root', '/bin/bash\n'])
-    False
+    Args:
+        user_data (list): a list comprised of user data from single line of /etc/passwd
     
+    Returns:
+        True (bool): returns if user_data is a user account 
+        False (bool): returns if user_data is not a user account
+
+    Example:
+        >>> is_user_account(['root', 'x', '0', '0', 'root', '/bin/bash\\n'])
+        False    
     '''
     
     if int(user_data[2]) >= 1000:
@@ -67,12 +78,15 @@ def is_user_account(user_data: list) -> bool:
 
 def get_user_accounts(passwd_file='/etc/passwd') -> list:
     '''
-    Parameter: file path (string) with a default of /etc/passwd
-    Returns: nested list of user account data (i.e. on element per user account that stores a list of all user account fields)
+    Args:
+        passwd_file (str): a file path (string) with a default of /etc/passwd
+
+    Returns: 
+        user_account_list (list): a nested list of user account data (i.e. one element per user account that stores a list of all user account fields)
     
-    >>> get_user_accounts('./pastawords.list')
-    [['bigmoocow', 'x', '1000', '1000', 'Big Moo Cow', '/home/bigmoocow', '/bin/bash\n'], ['littlemoocow', 'x', '1001', '1001', 'Little Moo Cow', '/home/littlemoocow', '/bin/bash\n']]
-    
+    Example:
+        >>> get_user_accounts('./passwd.truncated')
+        [['bigmoocow', 'x', '1000', '1000', 'Big Moo Cow', '/home/bigmoocow', '/bin/bash\\n'], ['littlemoocow', 'x', '1001', '1001', 'Little Moo Cow', '/home/littlemoocow', '/bin/bash\\n']]
     '''
     user_account_list = []
     listoflists = []
@@ -100,33 +114,37 @@ def get_sup_groups(user_data: list, groups: list) -> list:
     Matches a username from /etc/passwd to usernames in /etc/group to determine which groups a user belongs to
 
     Parameters:
-        user_data - single line of /etc/passwd as field seperated list
-        groups - nested list of field seperated group data
-    Returns: List of the supplimental groups that a user is part of
+        user_data (list): single line of /etc/passwd as field seperated list
+        groups (list): nested list of field separated group data
+
+    Returns:
+        sup_groups_list (list): a list of the supplemental groups that a user is part of
     
-    >>> get_sup_groups('bigmoocow:x:1000:1000:Big Moo Cow:/home/bigmoocow:/bin/bash', parse_groups('./parsely.list'))
-    ...
-    [['wheel', 'bigmoocow']]
+    Example:
+        >>> get_sup_groups('bigmoocow:x:1000:1000:Big Moo Cow:/home/bigmoocow:/bin/bash', parse_groups('./group.truncated'))
+        [['wheel', 'bigmoocow']]
     '''
     
     sup_groups_list = []
     sup_groups_list.append([line[0] for line in groups if (line[3].find(user_data[0])!=-1) ])
-    print(sup_groups_list)
     return sup_groups_list
 
 def gen_user_report(user_account_list: list, output_file='user_report.txt') -> None:
     '''
-    Parameters:
-        user account list
-        output file name with default
-    Returns: nothing
     This saves the user account list as text file in the specified format to the passed file name.
     This should be done with a for loop
+    
+    Args:
+        user_account_list (list): a list of lines from 
+        output_file (str): output file to write to, with default as 'user_report.txt'
+
+    Returns:
+        None
     '''
     with open (output_file, 'w') as output:
     
         for user in user_account_list:        
-            output.write("\n\nAccount Name: " + user[0])
+            output.write("Account Name: " + user[0])
             output.write("\nUID: " + user[2])              
             output.write("\nGID: " + user[3]) 
             output.write("\nHome Dir: " + user[5])
@@ -134,6 +152,7 @@ def gen_user_report(user_account_list: list, output_file='user_report.txt') -> N
         
             #prints user's groups
             output.write("Supplimentary Groups: " + str(get_sup_groups(user, parse_groups() )[0] ) )
+            output.write("\n\n")
 
     output.close()
     
@@ -144,5 +163,4 @@ def main():
     gen_user_report(get_user_accounts())
    
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
