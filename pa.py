52a58ada7b0964f96e9ed1e4679a8b60f27a3d08
Alec Spencer <aspencer18@my.bcit.ca>
2018-03-05 19:41:59 -0800

    pa.py v1.0.  Works well, but may need to be updated when requirements are clarified.

--- pa.py
+++ pa.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+
+###########################
+## pa.py v1.0			 ##
+## Alec Spencer			 ##
+## aspencer18@my.bcit.ca ##
+## March 5, 2018		 ##
+###########################
+
 import sys
 import os
 import subprocess
@@ -5,58 +14,100 @@ import json
 
 def writeJSON(mydictionary: dict()) -> None:
 	'''
-	writes a JSON object file to 'command.db'
+	writes a JSON object file to 'command_db.json'
 	'''
 	jsondict = json.dumps(mydictionary)
-	with open ('/home/aspencer/command.db', 'wt') as output:
+	with open ('command_db.json', 'wt') as output:
 		output.write(jsondict)
 	output.close()
 
+
 def readJSON() -> dict():
 	'''
-	reads 'command.db' and returns a JSON object file
+	reads 'command_db.json' and returns a JSON object file
 	'''
-	with open ('/home/aspencer/command.db', 'rt') as inputfile:
+	with open ('command_db.json', 'rt') as inputfile:
 		jsondict = inputfile.read()
-		mydictionary = json.loads(jsondict)
-		return mydictionary
+		return json.loads(jsondict)
 	inputfile.close()
 
-def processCMD(input: str, mydictionary: dict()) -> None:
-	if (input in str(mydictionary.values())):
+
+def processCMD(cmd: str, mydictionary: dict()) -> None:
+	'''
+	Prints the man pages for a command if it exists in the command dictionary.
+	If not, adds the command to the dictionary
+	'''
+	if (cmd in str(mydictionary.values())):
 		gethelp = subprocess.run(["man", sys.argv[2]], stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
 		print(gethelp.stdout)
-	#else:
-	#	pathdict[sys.argv[2]]
+	else:
+		#make a list of paths (dirsectories)
+		paths = os.getenv("PATH")
+		paths = paths.split(":")
+
+		try:
+				
+			for dirs in paths:
+				for filename in os.listdir(dirs):
+					fullname = os.path.join(dirs, filename)
+					if ((filename == cmd) and os.access(fullname, os.X_OK) and os.path.isfile(fullname)):
+						if dirs not in mydictionary:
+							mydictionary[dirs] = [cmd]
+						else:
+							mydictionary[dirs].append(cmd)
+		except:
+			pass
+
+		writeJSON(mydictionary)
+
+	return
+
 
 def FillDict() -> dict():
 	'''
-	reads all files in path locations and outputs a dictionary in the form of {'path directory': 'filename1', 'filename2', 'etc'}
+	reads all files in path locations and outputs a dictionary in the form of {'path dirsectory': 'filename1', 'filename2', 'etc'}
 	'''
-	#make a list of paths (directories)
+	#make a list of paths (dirsectories)
 	paths = os.getenv("PATH")
 	paths = paths.split(":")
 
-	pathdict = dict()
+	mydictionary = dict()
 
-	for dir in paths:
-		for file in os.listdir(dir):
-			fullname = os.path.join(dir, file)
-			#checks if file is executable and is a file, not a directory
-			if(os.access(fullname, os.X_OK) and os.path.isfile(fullname)):
-				pathdict[dir] = os.listdir(dir)
-	
-	return pathdict
+	try:
+		for dirs in paths:
+			for filename in os.listdir(dirs):
+				fullname = os.path.join(dirs, filename)
+				#checks if filename is executable and is a file, not a dirsectory
+				if(os.access(fullname, os.X_OK) and os.path.isfile(fullname)):
+					mydictionary[dirs] = os.listdir(dirs)
+	except:
+		pass
+
+	return mydictionary
 
 
-
-if (len(sys.argv) != 3):
-	print("incorrect number of args.  quitting.")
 
 def main():
-	#FillDict()
+
+	if (len(sys.argv) != 3):
+		print("Incorrect number of args.  \nSyntax:\n$ python pa.py cmd <command>\n$ python pa.py rep term\n$ python pa.py rep file")
+	else:
+	
+		pathdict = dict()
+		
+		try:
+			pathdict = readJSON()
+		except:
+			pass	
+
+		if sys.argv[1] == 'cmd':
+			processCMD(sys.argv[2], pathdict)
+		elif sys.argv[1] == 'rep':
+			if sys.argv[2] == 'term':
+				print(pathdict)
+			elif sys.argv[2] == 'file':
+				writeJSON(pathdict)
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
 
